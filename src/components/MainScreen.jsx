ab7e99d673fc20cf31e1d5b5a1633e8afff2542e
SethMcn <107069935+SethMcN@users.noreply.github.com>
2025-01-12 12:44:47 +0000

    Refactor MainScreen and Upgrades components for improved readability and consistency

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import Upgrades from './Upgrades';
+import Upgrades from "./Upgrades";
 export default function MainScreen() {
-  
-  const [CompanyName, setCompanyName] = useState('Burger Clicker');
+  const [CompanyName, setCompanyName] = useState("Burger Clicker");
 
   const [clicks, setClicks] = useState(100);
   const [clicksPerSec, setClicksPerSec] = useState(0);
@@ -14,74 +13,82 @@ export default function MainScreen() {
   const [RestaurantCount, setRestaurantCount] = useState(0);
   const [RestaurantCost, setRestaurantCost] = useState(100);
 
-  const [Upgradelist, setUpgradelist] = useState([[BurgerFlipperCount,BurgerFlipperCost], [RestaurantCount,RestaurantCost]]);
-
+  const [Upgradelist, setUpgradelist] = useState([
+    [BurgerFlipperCount, BurgerFlipperCost],
+    [RestaurantCount, RestaurantCost],
+  ]);
 
   useEffect(() => {
     if (clicksPerSec > 0) {
       const interval = setInterval(() => {
-        setClicks(prevClicks => prevClicks + 1);
+        setClicks((prevClicks) => prevClicks + 1);
       }, 1000 / clicksPerSec);
       return () => clearInterval(interval);
     }
   }, [clicks, clicksPerSec]);
 
-
-
   function handleUpgrade(upgradeClass, count, cost) {
-
     let isUpgradeSuccessful;
     if (clicks >= cost) {
+      if (upgradeClass === "burger-flipper") {
+        console.log("burger-flipper upgraded");
+        setBurgerFlipperCount((prevCount) => prevCount + 1);
+        setClicksPerSec((prevClicksPerSec) => prevClicksPerSec + 0.1);
+        setClicks((prevClicks) => prevClicks - cost);
+      }
 
-      if (upgradeClass === 'burger-flipper'){
-        console.log('burger-flipper upgraded');
-        setBurgerFlipperCount(prevCount => prevCount + 1);
-        setClicksPerSec(prevClicksPerSec => prevClicksPerSec + 0.1);
-        setClicks(prevClicks => prevClicks - cost);}
-        
-      if (upgradeClass === 'Restaurants'){
-        console.log('Restaurants upgraded')
-        setRestaurantCount(prevCount => prevCount + 1);
-        setClicksPerSec(prevClicksPerSec => prevClicksPerSec + 5);
-        setClicks(prevClicks => prevClicks - cost);}
-      
-      isUpgradeSuccessful = true;}
+      if (upgradeClass === "Restaurants") {
+        console.log("Restaurants upgraded");
+        setRestaurantCount((prevCount) => prevCount + 1);
+        setClicksPerSec((prevClicksPerSec) => prevClicksPerSec + 5);
+        setClicks((prevClicks) => prevClicks - cost);
+      }
 
-    else {
-      console.log('not enough burgers')
+      isUpgradeSuccessful = true;
+    } else {
+      console.log("not enough burgers");
       isUpgradeSuccessful = false;
     }
 
-    setUpgradelist([[BurgerFlipperCount, BurgerFlipperCost], [RestaurantCount, RestaurantCost]]);
+    setUpgradelist([
+      [BurgerFlipperCount, BurgerFlipperCost],
+      [RestaurantCount, RestaurantCost],
+    ]);
 
     return isUpgradeSuccessful;
-
   }
 
   const handleClick = () => {
-    setClicks(prevClicks => prevClicks + 1);
-  }
+    setClicks((prevClicks) => prevClicks + 1);
+  };
 
   return (
-    <div className='main-container'>
-      <div className='left'>
-        <div className='Company'>
-          <h1 className='CompanyName'>{CompanyName}</h1>
-          <i className="fa-solid fa-pen-to-square" id='EditName'></i>
+    <div className="main-container">
+      <div className="left">
+        <div className="Company">
+          <h1 className="CompanyName">{CompanyName}</h1>
+          <i className="fa-solid fa-pen-to-square" id="EditName"></i>
         </div>
-        <p className='click-counter'>{clicks} Burgers</p>
-        <p className='clicks-perSec'>{(clicksPerSec).toFixed(2)} Burgers per second</p>
+        <p className="click-counter">{clicks} Burgers</p>
+        <p className="clicks-perSec">
+          {clicksPerSec.toFixed(2)} Burgers per second
+        </p>
 
-        <div className='button-container'>
-          <img className='click-img' src="/src/components/freshBurger.png" alt="burger" onClick={() => { handleClick();}}/>
+        <div className="button-container">
+          <img
+            className="click-img"
+            src="src\assets\freshBurger.png"
+            alt="burger"
+            onClick={() => {
+              handleClick();
+            }}
+          />
         </div>
       </div>
-        
 
-      <div className='right'> 
-        <Upgrades handleUpgrade={handleUpgrade} Upgradelist={Upgradelist}/>
+      <div className="right">
+        <Upgrades handleUpgrade={handleUpgrade} Upgradelist={Upgradelist} />
       </div>
-
     </div>
   );
 }
--- src/components/Upgrades.jsx
+++ src/components/Upgrades.jsx
@@ -1,66 +1,116 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Upgrades(props) {
-    const handleUpgrade = props.handleUpgrade;
-    const BurgerFlippersData = props.Upgradelist[0];
-    const Restaurants = props.Upgradelist[1];
+  const handleUpgrade = props.handleUpgrade;
+  const BurgerFlippersData = props.Upgradelist[0];
+  const Restaurants = props.Upgradelist[1];
 
-    const [BurgerFlippersCountInternal, setBurgerFlippersCountInternal] = useState(BurgerFlippersData[0]);
-    const [BurgerFlippersCostInternal, setBurgerFlippersCostInternal] = useState(BurgerFlippersData[1]);
+  const [BurgerFlippersCountInternal, setBurgerFlippersCountInternal] =
+    useState(BurgerFlippersData[0]);
+  const [BurgerFlippersCostInternal, setBurgerFlippersCostInternal] = useState(
+    BurgerFlippersData[1]
+  );
 
-    const [RestaurantsCountInternal, setRestaurantsCountInternal] = useState(Restaurants[0]);
-    const [RestaurantsCostInternal, setRestaurantsCostInternal] = useState(Restaurants[1]);
+  const [RestaurantsCountInternal, setRestaurantsCountInternal] = useState(
+    Restaurants[0]
+  );
+  const [RestaurantsCostInternal, setRestaurantsCostInternal] = useState(
+    Restaurants[1]
+  );
 
-    // Synchronize internal state with props
-    useEffect(() => {
-        setBurgerFlippersCountInternal(BurgerFlippersData[0]);
-        setBurgerFlippersCostInternal(BurgerFlippersData[1]);
-        setRestaurantsCountInternal(Restaurants[0]);
-        setRestaurantsCostInternal(Restaurants[1]);
-    }, [props.Upgradelist]);
+  // Synchronize internal state with props
+  useEffect(() => {
+    setBurgerFlippersCountInternal(BurgerFlippersData[0]);
+    setBurgerFlippersCostInternal(BurgerFlippersData[1]);
+    setRestaurantsCountInternal(Restaurants[0]);
+    setRestaurantsCostInternal(Restaurants[1]);
+  }, [props.Upgradelist]);
 
-    const internalBurgerFlippersData = () => {
-        setBurgerFlippersCountInternal(BurgerFlippersCountInternal + 1);
-        setBurgerFlippersCostInternal(Math.round(BurgerFlippersCostInternal + (BurgerFlippersCostInternal * 0.1)));
-    };
-
-    const internalRestaurantsData = () => {
-        setRestaurantsCountInternal(RestaurantsCountInternal + 1);
-        setRestaurantsCostInternal(Math.round(RestaurantsCostInternal + (RestaurantsCostInternal * 0.1)));
-    };
-
-    function handleClick(upgradeClass, count, cost) {
-        if (handleUpgrade(upgradeClass, count, cost)) {
-            if (upgradeClass === 'burger-flipper') {
-                internalBurgerFlippersData();
-            } else if (upgradeClass === 'Restaurants') {
-                internalRestaurantsData();
-            }
-        }
-    }
-
-    return (
-        <div className="UpgradeContainer">
-            <div id='BurgerFlippers'>
-                <h1>Burger Flippers</h1>
-                <button onClick={() => handleClick('burger-flipper', BurgerFlippersCountInternal, BurgerFlippersCostInternal)}>Buy</button>
-                <p className='CountText'>count = {BurgerFlippersCountInternal}</p>
-                <p className='CostText'>cost = {BurgerFlippersCostInternal} <img src="src/components/freshBurger.png" alt="" /> </p>
-            </div>
-
-            <div id='Restaurants'>
-                <h1>Restaurants</h1>
-                <button onClick={() => handleClick("Restaurants", RestaurantsCountInternal, RestaurantsCostInternal)}>Buy</button>
-                <p className='CountText'>count = {RestaurantsCountInternal}</p>
-                <p className='CostText'>cost = {RestaurantsCostInternal} <img src="src/components/freshBurger.png" alt="" /> </p>
-            </div>
-
-            <div id='Burger Company'>
-                <h1>Burger Company</h1>
-                <button onClick={() => handleClick("Burger-Company", RestaurantsCountInternal, RestaurantsCostInternal)}>Buy</button>
-                <p className='CountText'>count = {RestaurantsCountInternal}</p>
-                <p className='CostText'>cost = {RestaurantsCostInternal} <img src="src/components/freshBurger.png" alt="" /> </p>
-            </div>
-        </div>
+  const internalBurgerFlippersData = () => {
+    setBurgerFlippersCountInternal(BurgerFlippersCountInternal + 1);
+    setBurgerFlippersCostInternal(
+      Math.round(BurgerFlippersCostInternal + BurgerFlippersCostInternal * 0.1)
     );
+  };
+
+  const internalRestaurantsData = () => {
+    setRestaurantsCountInternal(RestaurantsCountInternal + 1);
+    setRestaurantsCostInternal(
+      Math.round(RestaurantsCostInternal + RestaurantsCostInternal * 0.1)
+    );
+  };
+
+  function handleClick(upgradeClass, count, cost) {
+    if (handleUpgrade(upgradeClass, count, cost)) {
+      if (upgradeClass === "burger-flipper") {
+        internalBurgerFlippersData();
+      } else if (upgradeClass === "Restaurants") {
+        internalRestaurantsData();
+      }
+    }
+  }
+
+  return (
+    <div className="UpgradeContainer">
+      <div id="BurgerFlippers">
+        <h1>Burger Flippers</h1>
+        <button
+          onClick={() =>
+            handleClick(
+              "burger-flipper",
+              BurgerFlippersCountInternal,
+              BurgerFlippersCostInternal
+            )
+          }
+        >
+          Buy
+        </button>
+        <p className="CountText">count = {BurgerFlippersCountInternal}</p>
+        <p className="CostText">
+          cost = {BurgerFlippersCostInternal}{" "}
+          <img src="src\assets\freshBurger.png" alt="" />{" "}
+        </p>
+      </div>
+
+      <div id="Restaurants">
+        <h1>Restaurants</h1>
+        <button
+          onClick={() =>
+            handleClick(
+              "Restaurants",
+              RestaurantsCountInternal,
+              RestaurantsCostInternal
+            )
+          }
+        >
+          Buy
+        </button>
+        <p className="CountText">count = {RestaurantsCountInternal}</p>
+        <p className="CostText">
+          cost = {RestaurantsCostInternal}{" "}
+          <img src="src\assets\freshBurger.png" alt="" />{" "}
+        </p>
+      </div>
+
+      <div id="Burger Company">
+        <h1>Burger Company</h1>
+        <button
+          onClick={() =>
+            handleClick(
+              "Burger-Company",
+              RestaurantsCountInternal,
+              RestaurantsCostInternal
+            )
+          }
+        >
+          Buy
+        </button>
+        <p className="CountText">count = {RestaurantsCountInternal}</p>
+        <p className="CostText">
+          cost = {RestaurantsCostInternal}{" "}
+          <img src="src\assets\freshBurger.png" alt="" />{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
